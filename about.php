f3c15e0fc69c993a70274b64ab2282e287f83f0f
rafliamirudin <153246322+rafliamirudin@users.noreply.github.com>
2023-12-08 09:54:07 +0700

    commit 1

---
+++ about.php
@@ -0,0 +1,5 @@
+<?php include 'header.php'; ?>
+
+<h1> Ini Bagian About </h1>
+
+<?php include 'footer.php'; ?>
---
+++ index.php
@@ -0,0 +1,83 @@
+<?php include 'header.php'; ?>
+
+<!-- INI AKHIR NAVBAR -->
+
+<!-- JUMBOTRON -->
+<div class="jumbotron jumbotron-fluid">
+    <div class="container text-center">
+        <img class="ronded-circle" width="300" height="300" src="https://indonesia.go.id/assets/img/profile/profile.png" alt="">
+      <h1 class="display-4">Rafli Amirudin</h1>
+      <p class="lead">Menempuh Pendidikan Sarjana Bisnis Digital di Universitas Amikom Purwokerto</p>
+    </div>
+  </div>
+
+  <div class="container text-center" id="About">
+    <div class="row">
+        <div class="col">
+        <h1>About</h1>
+    </div>
+  </div>
+ 
+<div class="row">
+    <div class="col text-justify">
+        <P> Universitas Amikom Purwokerto merupakan perguruan tinggi yang berkonsentrasi dibidang Teknologi dan Bisnis Digital. Dengan sarana dan prasarana yang lengkap ditunjang dengan dosen dan praktisi yang telah berpengalaman didunia pendidikan dan industri, siap menjawab tantangan generasi Millenial memasuki dunia kerja pada era revolusi industri 4.0.
+            Universitas Amikom Purwokerto merupakan salah satu perguruan tinggi swasta di Purwokerto berbasis teknologi dan bisnis yang terletak di Jalan Letjend Pol. Soemarto Kabupaten Banyumas, Jawa Tengah. Tepatnya di depan Sekolah Polisi Negara Purwokerto.
+        </p>
+    </div>
+
+    <div class="col text-justify">
+        <p>Bisnis Digital Universitas Amikom Purwokerto merupakan program studi yg unggul dan diciptakan dengan mengikuti era teknologi saat ini. Menghasilkan bibit unggul dan prestasi luar biasa yang sangat membanggakan.
+           Bisnis digital atau digital business adalah penggunaan teknologi digital dalam upaya mengembangkan sebuah usaha, mulai dari model bisnisnya hingga pengalaman pelanggan. Digital business adalah bisnis yang telah menggunakan teknologi untuk menciptakan produk atau jasa, dan memasarkannya secara online melalui media digital, terutama internet.
+        </p>
+    </div>
+</div>
+
+  </div>
+
+
+  <div class="container text-center">
+    <div class="row">
+        <div class="col">
+        <h1>Portofolio</h1>
+    </div>
+  </div>
+
+  <!-- MENAMBAHKAN GAMBAR -->
+  <div class="card mt-5" style="width: 50rem;">
+    <img class="card-img-top" src="https://akupintar.id/documents/20143/0/Logo+Universitas+AMIKOM+Purwokerto.png/5396ccb8-55f7-fc6d-19bd-470483b718e4?version=1.0&t=1636016327521&imagePreview=1" alt="Card image cap">
+    <div class="card-body">
+      <p class="card-text">UNIVERSITAS AMIKOM PURWOKERTO</p>
+    </div>
+  </div>
+
+
+<!-- CARD DECKS -->
+  <div class="card-deck mt-5 ml-5 mr-5">
+    <div class="card">
+      <img class="card-img-top" src="https://bisdig.fbis.amikompurwokerto.ac.id/wp-content/uploads/2020/07/Logo-BD.png" alt="Card image cap">
+      <div class="card-body">
+        <h5 class="card-title">PROGRAM STUDI S1</h5>
+        <p class="card-text">BISNIS DIGITAL</p>
+        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+      </div>
+    </div>
+    <div class="card">
+      <img class="card-img-top" src="https://pmm.kampusmerdeka.kemdikbud.go.id/files/logopt/061054.png" alt="Card image cap">
+      <div class="card-body">
+        <h5 class="card-title"></h5>
+        <p class="card-text">UNIVERITAS AMIKOM PURWOKERTO</p>
+        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+      </div>
+    </div>
+    <div class="card">
+      <img class="card-img-top" src="https://bisdig.fbis.amikompurwokerto.ac.id/wp-content/uploads/2020/07/Logo-BD.png" alt="Card image cap">
+      <div class="card-body">
+        <h5 class="card-title">PROGRAM STUDI S1</h5>
+        <p class="card-text">BISNIS DIGITAL</p>
+        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+      </div>
+    </div>
+  </div>
+
+<?php include 'footer.php'; ?>
+  
